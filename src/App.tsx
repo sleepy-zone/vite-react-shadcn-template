721226325e04a94311c0362d8445f3a74afe30d9
Sleepy Zone <micro_sobby@outlook.com>
2024-03-27 09:18:55 +0000

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from './components/ui/button'
+import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
 
 import './App.css'
 
@@ -9,7 +11,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vrst-ui-theme">
       <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <ModeToggle />
       <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   )
 }
 
